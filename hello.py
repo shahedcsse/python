60910b41532397ae08e8428c2eda835dc8e444e3
shahedcsse <shahedhasan920@gmail.com>
2022-03-29 03:21:41 -0700

    add name

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print ("hello world")
+# print ("hello world")
+fname = input("Enter Your First Name: ")
+lname = input("Enter Your Last Name: ")
+#process phase
+fullname = fname  + lname
+
+# output phase -> Name!
+print(fullname)
